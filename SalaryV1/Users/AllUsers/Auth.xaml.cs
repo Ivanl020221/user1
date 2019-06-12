becfc568dce682fe6372e027f21540bcbd8c1f17
Иван Николаев <niksyper@gmail.com>
2019-06-12 22:23:45 +0300

    res 1

---
+++ SalaryV1/Users/AllUsers/Auth.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalaryV1.Users.AllUsers
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        Model.DataBaseConnectionModel ConnectionModel = new Model.DataBaseConnectionModel();
+
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn(object sender, RoutedEventArgs e)
+        {
+            if (ConnectionModel.manager.Where(i => i.login == log.Text && i.password == pass.Password).Count() > 0)
+            { NavigationService.Navigate(new Users.Manager.Main.MainPage()); return; }
+
+            if (ConnectionModel.executor.Where(i => i.login == log.Text && i.password == pass.Password).Count() > 0)
+            { NavigationService.Navigate(new Users.Executor.Main.MainPage()); return; }
+
+            MessageBox.Show("Не верный логин или пароль", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
--- SalaryV1/Utilites/Main.xaml.cs
+++ SalaryV1/Utilites/Main.xaml.cs
@@ -23,5 +23,38 @@ namespace SalaryV1.Utilites
         {
             InitializeComponent();
         }
+
+        private void SetTitle(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (sender is Page page)
+            {
+                this.Title = $"Распределение зарплат - {page.Title}";
+            }
+        }
+
+        private void ExitThis(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.
+                Show(
+                "Вы уверены, " +
+                "что хотите выйти?",
+                "Выход",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question) ==
+                MessageBoxResult.Yes)
+            {
+                Main main = new Main();
+                main.Show();
+                this.Close();
+            }
+        }
+
+        private void GoBack(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
     }
 }
